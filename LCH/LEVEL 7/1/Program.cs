36246ac3673120a17eece002e5e2d4a4f52157eb
CheongHo-Lee <79739183+CheongHo-Lee@users.noreply.github.com>
2021-12-08 16:15:54 +0900

    문자열-아스키코드

---
+++ LCH/LEVEL 7/1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(Convert.ToInt32(input));
+        }
+    }
+}
