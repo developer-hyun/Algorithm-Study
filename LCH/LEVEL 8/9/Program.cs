39384f9acf9ef95085224d746508584a23cb51dc
이청호 <uirobol@gmail.com>
2021-12-19 00:24:53 +0900

    8-9

---
+++ LCH/LEVEL 8/9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                string input = Console.ReadLine();
+                string[] inputData = input.Split(' ');
+                long x = Convert.ToInt32(inputData[0].ToString());
+                long y = Convert.ToInt32(inputData[1].ToString());
+
+                long an = 2;
+                long k = 2;
+                long d = y - x;
+                long count = 1;
+                long output = 0;
+                while (d > an)
+                {
+                    an += 2 * k;
+                    k += 1;
+                    count += 1;
+                }
+
+                if (an - count < d)
+                    output = count * 2;
+                else
+                    output = (count * 2) - 1;
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
