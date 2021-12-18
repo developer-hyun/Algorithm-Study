822da879af8f6a0ed5cb088f070ed9bbaa75310e
이청호 <uirobol@gmail.com>
2021-12-19 01:13:31 +0900

    .

--- LCH/LEVEL 8/9/Program.cs
+++ LCH/LEVEL 8/9/Program.cs
@@ -14,19 +14,20 @@ namespace _9
                 long x = Convert.ToInt32(inputData[0].ToString());
                 long y = Convert.ToInt32(inputData[1].ToString());
 
-                long an = 2;
-                long k = 2;
-                long d = y - x;
-                long count = 1;
+                long an = 2; //a1
+                long k = 2; //계차수열
+                long d = y - x; //거리
+                long count = 1; 
                 long output = 0;
+
                 while (d > an)
                 {
                     an += 2 * k;
                     k += 1;
-                    count += 1;
+                    count += 1; 
                 }
 
-                if (an - count < d)
+                if (an - count < d) 
                     output = count * 2;
                 else
                     output = (count * 2) - 1;
