baade8b55d27bbd4a2ecbab0ec4255367ec0d66e
nueob <79954748+nueob@users.noreply.github.com>
2021-12-18 16:48:00 +0900

    백준 2869 달팽이는 올라가고싶다

--- LBE/src/Baekjoon_Math1/Snail.java
+++ LBE/src/Baekjoon_Math1/Snail.java
@@ -1,7 +1,4 @@
-package com.company;
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
+package Baekjoon_Math1;
 //import java.util.Scanner;
 //
 //public class Main {
@@ -24,15 +21,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Snail {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         int up = Integer.parseInt(st.nextToken());
         int down = Integer.parseInt(st.nextToken());
         int tree_length = Integer.parseInt(st.nextToken());
         int start = (tree_length - up);
         int day = up-down;
+        
         if(start == 0) {
             System.out.println(1);
         } else {
