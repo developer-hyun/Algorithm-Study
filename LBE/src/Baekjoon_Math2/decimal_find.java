0a4b68728223b88c2a42ac5105248145583c1920
nueob <79954748+nueob@users.noreply.github.com>
2021-12-25 21:11:55 +0900

    백준 1978번 소수찾기

--- LBE/src/Baekjoon_Math2/decimal_find.java
+++ LBE/src/Baekjoon_Math2/decimal_find.java
@@ -1,7 +1,6 @@
-package com.company;
+package Baekjoon_Math2;
 import java.util.Scanner;
-
-public class Main {
+public class decimal_find {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int case_cnt = sc.nextInt();
