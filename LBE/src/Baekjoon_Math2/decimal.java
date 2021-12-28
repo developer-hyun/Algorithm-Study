16337b1dc51cc675814f1a5f97ed2e19b0432293
nueob <79954748+nueob@users.noreply.github.com>
2021-12-28 14:20:03 +0900

    백준 2581번 소수

--- LBE/src/Baekjoon_Math2/decimal.java
+++ LBE/src/Baekjoon_Math2/decimal.java
@@ -1,8 +1,9 @@
-package com.company;
+package Baekjoon_Math2;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class decimal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int M = sc.nextInt();
