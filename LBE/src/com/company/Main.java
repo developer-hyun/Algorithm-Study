b9372074804bf448f72ac745b21f2669ef33faf2
nueob <79954748+nueob@users.noreply.github.com>
2021-12-28 21:00:29 +0900

    백준 4948 베르트랑 공준

--- LBE/src/com/company/Main.java
+++ LBE/src/com/company/Main.java
@@ -1,28 +1,24 @@
 package com.company;
-/*
-    에라토스테네스의 체 : 소수(prime number) 판별 알고리즘
-    ex ) 8의 경우 2*4 = 4*2 와 같은 대칭을 이루기 때문에
-    제곱근까지만 약수의 여부를 검증하면 됨
-*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
-        String[] s = br.readLine().split(" ");
-        int m = Integer.parseInt(s[0]);
-        int n = Integer.parseInt(s[1]);
-        for(int i=m;i<=n;i++) {
-            if(i!= 1) {
-                if(!check_not_decimal(i)) {
-                    sb.append(i).append("\n");
+        while(true) {
+            int n = Integer.parseInt(br.readLine());
+            if(n == 0) break;
+            int cnt = 0;
+            for(int i=n+1;i<=2*n;i++) {
+                if(!check_not_decimal(i)){
+                    cnt++;
                 }
             }
+            System.out.println(cnt);
         }
-        System.out.println(sb);
     }
     public static boolean check_not_decimal(int n) {
         for(int i=2; i<=Math.sqrt(n);i++) {
