437ff770a39f7909898d6d9ded38c44d6690a562
nueob <79954748+nueob@users.noreply.github.com>
2021-12-28 15:58:02 +0900

    백준 11653번 소인수분해

--- LBE/src/Baekjoon_Math2/Factorization.java
+++ LBE/src/Baekjoon_Math2/Factorization.java
@@ -1,10 +1,11 @@
-package com.company;
+package Baekjoon_Math2;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class Main {
+public class Factorization {
     public static void main(String[] args) throws IOException {
 //        long start = System.currentTimeMillis();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
