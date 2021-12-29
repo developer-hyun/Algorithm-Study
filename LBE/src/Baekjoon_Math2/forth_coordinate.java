d2a3055636b62c8aa6f09667543d06c862f05093
nueob <79954748+nueob@users.noreply.github.com>
2021-12-29 18:13:40 +0900

    백준 3009번 네 번째 점

--- LBE/src/Baekjoon_Math2/forth_coordinate.java
+++ LBE/src/Baekjoon_Math2/forth_coordinate.java
@@ -1,10 +1,10 @@
-package com.company;
+package Baekjoon_Math2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class forth_coordinate {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
