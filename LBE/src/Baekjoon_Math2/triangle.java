da3203f27edaa10a827d2f491e471f449591a8b0
nueob <79954748+nueob@users.noreply.github.com>
2021-12-29 18:28:25 +0900

    백준 4153번 직각삼각형

--- LBE/src/Baekjoon_Math2/triangle.java
+++ LBE/src/Baekjoon_Math2/triangle.java
@@ -1,13 +1,12 @@
-package com.company;
+package Baekjoon_Math2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class triangle {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
         while(true) {
             String[] coordinate = br.readLine().split(" ");
             if(Integer.parseInt(coordinate[0]) == 0 && Integer.parseInt(coordinate[1]) == 0 && Integer.parseInt(coordinate[2]) == 0) break;
