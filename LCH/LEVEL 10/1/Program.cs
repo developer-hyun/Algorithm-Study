eb055b6f214da5e98714c5da95cae2f15bf5c60f
이청호 <uirobol@gmail.com>
2021-12-30 17:59:03 +0900

    10-2

---
+++ LCH/LEVEL 10/1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 1;
+            int output = Factorial(N, sum);
+            Console.WriteLine("output : " + output);
+        }
+
+        public static int Factorial(int N, int sum)
+        {
+            if (N <= 0)
+            {
+                N = 1;
+                sum *= N;
+                return sum;
+            }
+            sum *= N;
+
+            return Factorial((N-1), sum);
+        }
+    }
+}
---
+++ LCH/LEVEL 10/2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int F = 0;
+            int FMinus1 = 0;
+            int FMinus2 = 1;
+            int count = 1;
+            int output = Fibonacci(N, F, count, FMinus1, FMinus2);
+
+            Console.WriteLine(output);
+        }
+
+        public static int Fibonacci(int N, int F, int count, int FMinus1,  int FMinus2)
+        {
+            if (N == 0)
+                return 0;
+            else if (N == 1)
+                return 1;
+
+            if (count == N)
+                return F;
+
+            F = FMinus1 + FMinus2;
+            FMinus1 = FMinus2;
+            FMinus2 = F;
+            count += 1;
+            Console.WriteLine("F : {0}, FMinus1 : {1}, FMinus2 : {2}, count : {3}", F, FMinus1 ,FMinus2, count);
+
+            return Fibonacci(N, F, count, FMinus1, FMinus2);
+        }
+    }
+
+}
