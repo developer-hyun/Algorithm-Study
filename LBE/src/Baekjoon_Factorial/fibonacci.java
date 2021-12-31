d761a899c617724f0b9401b5d62f750b9f1c869b
nueob <79954748+nueob@users.noreply.github.com>
2021-12-31 21:21:34 +0900

    백준 10870번 피보나치 수열

--- LBE/src/Baekjoon_Factorial/fibonacci.java
+++ LBE/src/Baekjoon_Factorial/fibonacci.java
@@ -1,18 +1,18 @@
-package com.company;
+package Baekjoon_Factorial;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class fibonacci {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        System.out.println(fibonacci(n));
+        if(n == 0) System.out.println(1);
+        else System.out.println(factorial(n));
     }
-    public static int fibonacci (int n) {
-        if(n<=1) return n;
-        else
-            return fibonacci(n - 2) + fibonacci(n - 1);
+    public static int factorial (int n) {
+        if(n <= 1) return n;
+        else return n*factorial(n-1);
     }
 }
