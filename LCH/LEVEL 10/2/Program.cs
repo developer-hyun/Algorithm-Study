b76014350f45ac79128f57570c58e168fb874249
이청호 <uirobol@gmail.com>
2021-12-31 23:43:59 +0900

    10-3, 10-4, 9-4

--- LCH/LEVEL 10/2/Program.cs
+++ LCH/LEVEL 10/2/Program.cs
@@ -16,7 +16,7 @@ namespace _2
             Console.WriteLine(output);
         }
 
-        public static int Fibonacci(int N, int F, int count, int FMinus1,  int FMinus2)
+        public static int Fibonacci(int N, int F, int count, int FMinus2,  int FMinus1)
         {
             if (N == 0)
                 return 0;
@@ -29,9 +29,9 @@ namespace _2
             F = FMinus1 + FMinus2;
             count += 1;
             Console.WriteLine("F : {0}, FMinus1 : {1}, FMinus2 : {2}, count : {3}", F, FMinus1 ,FMinus2, count);
-            FMinus1 = FMinus2;
-            FMinus2 = F;
-            return Fibonacci(N, F, count, FMinus1, FMinus2);
+            FMinus2 = FMinus1;
+            FMinus1 = F;
+            return Fibonacci(N, F, count, FMinus2, FMinus1);
         }
     }
 
