0a2a31daedb16ead35a331eae4cedec34e853519
nueob <79954748+nueob@users.noreply.github.com>
2022-01-01 22:54:43 +0900

    백준 9020번 골드바흐의 추측

--- LBE/src/com/company/Main.java
+++ LBE/src/com/company/Main.java
@@ -7,12 +7,29 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        System.out.println(fibonacci(n));
+        StringBuffer sb = new StringBuffer();
+        int cnt = Integer.parseInt(br.readLine());
+        while(cnt > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int p = n/2;
+            int q = n/2;
+            for(int i=0;i<n/2;i++) {
+                if(!check_not_decimal(p) && !check_not_decimal(q)) {
+                    System.out.println(q+ " " + p);
+                    break;
+                }
+                p++;
+                q--;
+            }
+            cnt--;
+        }
     }
-    public static int fibonacci (int n) {
-        if(n<=1) return n;
-        else
-            return fibonacci(n - 2) + fibonacci(n - 1);
+    public static boolean check_not_decimal(int n) {
+        for(int i=2; i<=Math.sqrt(n);i++) {
+            if(n % i == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
