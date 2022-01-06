434e8b49b37b4379d40e7763a77aaea1f89237c2
nueob <79954748+nueob@users.noreply.github.com>
2022-01-06 23:03:32 +0900

    백준 1181번 단어 정렬

--- 3. LBE/src/Beakjoon_Sort/word_sort.java
+++ 3. LBE/src/Beakjoon_Sort/word_sort.java
@@ -1,10 +1,10 @@
-package com.company;
+package Beakjoon_Sort;
 
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class Main{
+public class word_sort{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -31,4 +31,3 @@ public class Main{
         }
     }
 }
-
