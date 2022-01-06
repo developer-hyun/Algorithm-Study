09bdfff02597315d0dec0957e7bafa450e990d68
이청호 <uirobol@gmail.com>
2022-01-06 19:25:45 +0900

    12-3

--- 1. CheongHo LEE/LEVEL 12/3/3/Program.cs
+++ 1. CheongHo LEE/LEVEL 12/3/3/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace _3
+namespace PracticeAlgo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            StreamReader reader = new StreamReader(Console.OpenStandardInput());
+            StreamWriter print = new StreamWriter(Console.OpenStandardOutput());
+
+            int count = Convert.ToInt32(reader.ReadLine());
+            int[] list = new int[10001];
+            for (int i = 0; i < count; i++)
+            {
+                list[int.Parse(reader.ReadLine())]++;
+            }
+            for (int i = 0; i <= 10000; i++)
+            {
+                if (list[i] != 0)
+                {
+                    for (int j = 0; j < list[i]; j++)
+                    {
+                        print.WriteLine(i);
+                    }
+                }
+            }
         }
     }
 }
