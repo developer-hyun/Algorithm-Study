70b1a7ad6923563b4e210673a85b06b275e5a417
nueob <79954748+nueob@users.noreply.github.com>
2022-01-07 23:38:16 +0900

    백준 11650번 좌표정렬하기

--- 3. LBE/src/Beakjoon_Sort/location_sort_1.java
+++ 3. LBE/src/Beakjoon_Sort/location_sort_1.java
@@ -1,10 +1,10 @@
-package com.company;
+package Beakjoon_Sort;
 
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class Main{
+public class location_sort_1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
