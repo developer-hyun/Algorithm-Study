a5e237c04ad136ef1783be074f905555f09aee89
이청호 <uirobol@gmail.com>
2022-01-07 16:45:46 +0900

    11-5수정

--- 1. CheongHo LEE/LEVEL 11/5/5/Program.cs
+++ 1. CheongHo LEE/LEVEL 11/5/5/Program.cs
@@ -12,21 +12,75 @@ namespace _5
         {
             int N = Convert.ToInt32(Console.ReadLine());
             int count = 0;
-            int defaultValue = 666;       
-            
+            int defaultValue = 666;
+
             while (N > count)
             {
-                if(defaultValue.ToString().Contains("666"))
+                if (defaultValue.ToString().Contains("666"))
                 {
                     count += 1;
                 }
                 defaultValue += 1;
             }
-            
 
-            Console.WriteLine(defaultValue-1);
+
+            Console.WriteLine(defaultValue - 1);
 
             Console.ReadKey();
+            
         }
     }
+
+    //if(N == 1)
+    //    Console.WriteLine(defaultValue);
+
+    //while (N > check)
+    //{
+    //    check += 19;
+    //    checkCount += 1;
+    //}
+
+    //defaultValue = defaultValue + 10000 * (checkCount - 1);
+    //N = N - 19 * (checkCount - 1);
+
+
+    //while (N > count)
+    //{
+    //    if (count < 6)
+    //    {
+    //        defaultValue += 1000;
+    //        count += 1;
+    //        continue;
+    //    }
+    //    else if (count == 6)
+    //    {
+    //        defaultValue += 994;
+    //        count += 1;
+    //        continue;
+    //    }
+    //    else if (count > 6 && count < 16)
+    //    {
+    //        defaultValue += 1;
+    //        count += 1;
+    //        continue;
+    //    }
+    //    else if (count == 16)
+    //    {
+    //        defaultValue += 997;
+    //        count += 1;
+    //        continue;
+    //    }
+    //    else if (count > 16 && count < 19)
+    //    {
+    //        defaultValue += 1000;
+    //        count += 1;
+    //        continue;
+    //    }
+
+    //}
+
+
+    //Console.WriteLine(defaultValue);
+
+        
 }
