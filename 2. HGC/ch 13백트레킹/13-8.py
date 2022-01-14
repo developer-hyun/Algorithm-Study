4c4938f1a9b3f999ad2172c062f75284fd15314f
developer-hyun <rlckdrkdud@naver.com>
2022-01-14 13:40:11 +0900

    0114

---
+++ 2. HGC/ch 13백트레킹/13-8.py
@@ -0,0 +1,25 @@
+#스타트와 링크
+
+
+
+def backtracking(cnt):
+    
+
+
+
+
+
+
+
+
+
+
+#
+if __name__ == '__main__':
+    import sys
+
+    N = int(sys.stdin.readline())
+    dataList=[]
+    for i in range(N):  #이중리스트로 받음
+        dataList.append(list(map(int,sys.stdin.readline().split())))
+    #print(dataList)
