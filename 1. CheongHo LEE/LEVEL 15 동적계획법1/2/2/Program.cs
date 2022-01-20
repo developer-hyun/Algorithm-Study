e02cfa5dc0d41516675a2f7c0988a9cbb4086952
이청호 <uirobol@gmail.com>
2022-01-20 22:02:59 +0900

    15-3

--- 1. CheongHo LEE/LEVEL 15 동적계획법1/2/2/Program.cs
+++ 1. CheongHo LEE/LEVEL 15 동적계획법1/2/2/Program.cs
@@ -8,12 +8,13 @@ namespace _2
 {
     internal class Program
     {
-        static int[,,] arr = new int[101,101,101];
-        static string[] intputdata;
+        static int[,,] arr = new int[101, 101, 101];
+        static string[] inputdata;
         static int a, b, c;
-        static string intput;
+        static string input;
         static int w(int a, int b, int c)
         {
+            // Console.WriteLine(string.Format("{0}, {1}. {2}", a, b, c));
             if (arr[a, b, c] != 0)
             {
                 return arr[a, b, c];
@@ -21,18 +22,17 @@ namespace _2
 
             if (a <= 50 || b <= 50 || c <= 50)
             {
-                arr[a, b, c] = 1;
-                return 1;
+                return arr[a, b, c] = 1;
             }
             else if (a > 70 || b > 70 || c > 70)
-                return w(70, 70, 70);
+                w(70, 70, 70);
             else if (a < b && b < c)
             {
                 arr[a, b, c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
             }
             else
             {
-                arr[a,b,c] = w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+                arr[a, b, c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
             }
             return arr[a, b, c];
         }
@@ -40,19 +40,31 @@ namespace _2
         {
             while (true)
             {
-                intput = Console.ReadLine();
-                if (intput == "-1 -1 -1")
+                input = Console.ReadLine();
+                inputdata = input.Split(' ');
+                if (inputdata[0] == "-1" && inputdata[1] == "-1" && inputdata[2] == "-1")
                     break;
-                intputdata = intput.Split(' ');
-                a = Convert.ToInt32(intputdata[0]) + 50;
-                b = Convert.ToInt32(intputdata[1]) + 50;
-                c = Convert.ToInt32(intputdata[2]) + 50;
+
+                a = Convert.ToInt32(inputdata[0]) + 50;
+                b = Convert.ToInt32(inputdata[1]) + 50;
+                c = Convert.ToInt32(inputdata[2]) + 50;
 
 
-                arr[a, b, c] = w(a, b, c);
-                Console.WriteLine(string.Format("w({0}, {1}, {2}) = {3}", intputdata[0], intputdata[1], intputdata[2], arr[a, b, c]));
+                if (a > 70 || b > 70 || c > 70)
+                {
+                    //Console.WriteLine(string.Format("70보다 크네 {0}, {1}. {2}", a, b, c));
+                    arr[70, 70, 70] = w(70, 70, 70);
+                    Console.WriteLine(string.Format("w({0}, {1}, {2}) = {3}", inputdata[0], inputdata[1], inputdata[2], arr[70, 70, 70]));
+                    //Console.WriteLine("1");
+                }
+                else
+                {
+                    arr[a, b, c] = w(a, b, c);
+                    Console.WriteLine(string.Format("w({0}, {1}, {2}) = {3}", inputdata[0], inputdata[1], inputdata[2], arr[a, b, c]));
+                    //Console.WriteLine("2");
+                }
+
             }
-            Console.ReadKey();
         }
     }
 }
