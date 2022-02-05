4a5df087092cc5473a1b6d91b55b61af6deacba8
boeun1 <79954748+nueob@users.noreply.github.com>
2022-02-06 01:38:34 +0900

    백준 13305번 주유소

--- 3. LBE/src/Beakjoon_Greedy/GasStation.java
+++ 3. LBE/src/Beakjoon_Greedy/GasStation.java
@@ -1,11 +1,11 @@
-package com.company;
+package Beakjoon_Greedy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class GasStation {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
