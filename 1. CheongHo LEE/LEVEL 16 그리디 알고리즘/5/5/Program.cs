a6c3f62e73f29115c7da1884f59b47f513b92b93
이청호 <uirobol@gmail.com>
2022-02-06 01:43:54 +0900

    16-5

--- 1. CheongHo LEE/LEVEL 16 그리디 알고리즘/5/5/Program.cs
+++ 1. CheongHo LEE/LEVEL 16 그리디 알고리즘/5/5/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Text;
 
 namespace _5
 {
@@ -6,7 +9,28 @@ namespace _5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] d = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int[] won = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            //최소 주유 가격
+            int minWon = won[0];
+            //결과
+            BigInteger output = new BigInteger();                         
+
+            for (int i = 0; i < d.Length; i++)
+            {
+                if(minWon < won[i])
+                {
+                    output += BigInteger.Parse(minWon.ToString()) * BigInteger.Parse(d[i].ToString());
+                }
+                else
+                {
+                    output += BigInteger.Parse(won[i].ToString()) * BigInteger.Parse(d[i].ToString());
+                    minWon = won[i];
+                }
+            }         
+            Console.WriteLine(output);
         }
     }
 }
