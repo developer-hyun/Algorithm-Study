6ad66a99e3c8a5a34a35da864e944455dbe887da
boeun1 <79954748+nueob@users.noreply.github.com>
2022-02-05 23:48:10 +0900

    백준 1541번 잃어버린 괄호

--- 3. LBE/src/Beakjoon_Greedy/Bracket.java
+++ 3. LBE/src/Beakjoon_Greedy/Bracket.java
@@ -1,12 +1,10 @@
-package com.company;
+package Beakjoon_Greedy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
-public class Main {
+public class Bracket {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String expression = br.readLine();
