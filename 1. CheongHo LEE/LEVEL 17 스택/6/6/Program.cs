cede32fcfe64f2b409cb39bcb1e69b53e7f7f27b
이청호 <uirobol@gmail.com>
2022-02-11 02:10:45 +0900

    17-6

--- 1. CheongHo LEE/LEVEL 17 스택/6/6/Program.cs
+++ 1. CheongHo LEE/LEVEL 17 스택/6/6/Program.cs
@@ -1,27 +1,47 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+
 namespace _6
 {
     internal class Program
     {
-        static int N = Convert.ToInt32(Console.ReadLine());
-        static int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-
-        static StringBuilder sb = new StringBuilder();
-        static Stack<int> stack = new Stack<int>();
         static void Main(string[] args)
         {
 
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            StringBuilder sb = new StringBuilder();
+            Stack<int> stack = new Stack<int>();
+            Stack<int> stackIndex = new Stack<int>();
+            int[] outputArr = new int[N];
+
             for (int i = 0; i < N; i++)
             {
-                stack.Push(input[i]);
+                int idx = i;
+                while(stack.Count > 0 && (stack.Peek() < input[idx]))
+                {
+                    outputArr[stackIndex.Peek()] = input[idx];
+                    stack.Pop();
+                    stackIndex.Pop();
+                }
+                stack.Push(input[idx]);
+                stackIndex.Push(idx);
+                
             }
-        }
+            while(stackIndex.Count > 0)
+            {
+                outputArr[stackIndex.Peek()] = -1;
+                stackIndex.Pop();
+            }
 
-        static public void NGE()
-        {
-
-        }
+            outputArr[N-1] = -1;
+            foreach (var item in outputArr)
+            {
+                sb.Append(item + " ");
+            }
+            Console.WriteLine(sb);
+        }      
     }
 }
