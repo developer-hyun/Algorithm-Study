e076aed1e7c4b084a076b5133233a8a8e7d1ee06
boeun1 <79954748+nueob@users.noreply.github.com>
2022-02-10 23:08:25 +0900

    백분 10773번 제로

---
+++ 3. LBE/src/Stack/Stack.java
@@ -0,0 +1,39 @@
+package Stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Stack {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            String order = st.nextToken();
+            switch (order) {
+                case "push" :
+                    int value = Integer.parseInt(st.nextToken());
+                    stack.push(value);
+                    break;
+                case "pop" :
+                    if(stack.empty()) System.out.println("-1");
+                    else System.out.println(stack.pop());
+                    break;
+                case "size" :
+                    System.out.println(stack.size());
+                    break;
+                case "empty" :
+                    if(stack.empty()) System.out.println(1);
+                    else System.out.println(0);
+                    break;
+                case "top" :
+                    if(stack.empty()) System.out.println(-1);
+                    else System.out.println(stack.peek());
+                    break;
+            }
+        }
+    }
+}
--- 3. LBE/src/Stack/Zero.java
+++ 3. LBE/src/Stack/Zero.java
@@ -1,9 +1,12 @@
-package com.company;
+package Stack;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Zero {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Stack<Integer> stack = new Stack<>();
