dd14bdce40ab04bc46460bdaa5123e1b138cacaf
이청호 <uirobol@gmail.com>
2022-02-11 14:14:45 +0900

    18-2

---
+++ 1. CheongHo LEE/LEVEL 18 큐,덱/2/2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            Queue<int> q = new Queue<int>();
+
+            //카드를 버릴건지 밑으로 옮길건지
+            //확인하기 위한 bool
+            bool cardOut = false;
+
+            int card = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                q.Enqueue(i);
+            }
+            while(q.Count > 1)
+            {
+                if(!cardOut)
+                {
+                    q.Dequeue();
+                    cardOut = true;
+                }
+                else
+                {
+                    card = q.Dequeue();
+                    q.Enqueue(card);
+                    cardOut = false;
+                }
+            }
+            Console.WriteLine(q.Dequeue());
+        }
+    }
+}
