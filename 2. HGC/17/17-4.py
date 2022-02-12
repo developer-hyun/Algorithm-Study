bb2b3d29798fdd72506b60b040879ce818bcc556
developer-hyun <rlckdrkdud@naver.com>
2022-02-13 00:26:39 +0900

    0213

--- 2. HGC/17/17-4.py
+++ 2. HGC/17/17-4.py
@@ -1,5 +1,25 @@
 
+import sys
+from collections import deque
+
+N = int(input())
+for i in range(N):
+    n,m = map(int,input().split())
+    q = deque(list(map(int,sys.stdin.readline().split())))
+    count=0
+    while q:
+        maxNumber = max(q)
+        nowQNumber = q.popleft()
+        m -=1 # 내 위치가 하나를 빼서 앞당겨짐
+
+        if maxNumber == nowQNumber:
+            count +=1
+            if m<0:
+                print(count)
+                break
+        else:
+            q.append(nowQNumber)
+            if m<0:
+                m = len(q) -1 #맨뒤로 이동하면 m값을 조정해줘야함
 
 
-
-if __name__ == '__main__':
