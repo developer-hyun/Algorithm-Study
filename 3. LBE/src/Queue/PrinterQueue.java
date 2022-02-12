b436c27461a1b15a4ca675f91bf962799b1aa079
boeun1 <79954748+nueob@users.noreply.github.com>
2022-02-13 00:20:44 +0900

    백준 1966번 프린터 큐

--- 3. LBE/src/Queue/PrinterQueue.java
+++ 3. LBE/src/Queue/PrinterQueue.java
@@ -1,9 +1,13 @@
-package com.company;
+package Queue;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
+public class PrinterQueue {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
