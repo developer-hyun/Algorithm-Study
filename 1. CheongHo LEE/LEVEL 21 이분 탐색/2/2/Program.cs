f6aae11fd4c750501e931ac373574ba095b5cec6
이청호 <uirobol@gmail.com>
2022-02-18 17:12:37 +0900

    21-2

--- 1. CheongHo LEE/LEVEL 21 이분 탐색/2/2/Program.cs
+++ 1. CheongHo LEE/LEVEL 21 이분 탐색/2/2/Program.cs
@@ -11,40 +11,28 @@ namespace _2
         static int[] M_Array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
         static StringBuilder sb = new StringBuilder();
 
-        static public void BinarySearch()
-        {
 
-            Array.Sort(A); // A = {1 2 3 4 5}
-            for (int i = 0; i < M; i++)
-            {
-                int index = Array.BinarySearch(A,0,A.Length, M_Array[i]);
-                //Console.WriteLine("index : " + index);
-                if (index < 0)
-                {
-                    sb.Append("0 ");
-                }
-                else
-                {
-                    sb.Append(index+ " ");
-                }
-
-                //내가 찾고자 하는 값
-                int target = M_Array[i];
-                //이진탐색의 시작점
-                int start = 0;
-                //이진탐색의 끝점
-                int end = A.Length - 1;
-                //이진탐색의 가운데점
-                int mid = (start + end) / 2;
-
-            }
-
-        }
         static void Main(string[] args)
         {
-            BinarySearch();
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] A = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int M = Convert.ToInt32(Console.ReadLine());
+            int[] M_Array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            StringBuilder sb = new StringBuilder();
+
+            int[] array = new int[20000001];
+            for (int i = 0; i < N; i++)
+            {
+                int tmp = A[i] + 10000000;
+                array[tmp]++;
+            }
+            for (int i = 0; i < M; i++)
+            {
+                int tmp = M_Array[i] + 10000000;
+                sb.Append(array[tmp] + " ");
+            }
+
             Console.WriteLine(sb);
         }
     }
 }
-
