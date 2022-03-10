c30122d4f809302fb63413ca9282aaf14ed949d9
이청호 <uirobol@gmail.com>
2022-03-10 17:09:19 +0900

    24-4

--- 1. CheongHo LEE/LEVEL 24 DFS와 BFS/4/Program.cs
+++ 1. CheongHo LEE/LEVEL 24 DFS와 BFS/4/Program.cs
@@ -4,46 +4,41 @@ namespace _4
 {
     class Program
     {
-        static int N;
+        static int T,M,N,K;
 
         //맵 저장 행렬
-        static int[,] map = new int[26, 26];
-        //단지내의 집의 수 저장 행렬
-        static int[] houseCount = new int[1000];
+        static int[,] map = new int[51, 51];
         //방문 확인 행렬
-        static bool[,] visited = new bool[26, 26];
+        static bool[,] visited = new bool[51, 51];
 
         static Queue<(int, int)> queue = new Queue<(int, int)>();
 
         static int[] dx = { -1, 1, 0, 0 };
         static int[] dy = { 0, 0, -1, 1 };
 
-        //단지수
-        static int count = 0;
-        //단지내의 집수
-        static int count2 = 0;
+        //배추흰지렁이 수
+        static int count;
 
         static void Reset() //초기화
-        {
-            for (int i = 1; i <= N; i++)
+        {            
+            count = 0;
+            for (int i = 0; i < M; i++)
             {
-                for (int j = 1; j <= N; j++)
+                for (int j = 0; j < N; j++)
                 {
                     visited[i, j] = false;
+                    map[i, j] = 0;
                 }
             }
         }
-        static void BFS(int x, int y, int count)
+        static void BFS(int x, int y)
         {
-            count2 = 0;
             queue.Enqueue((x, y));
             visited[x, y] = true;
 
             while (queue.Count != 0)
             {
                 var cur = queue.Dequeue();
-                //pop할때마다 집의 수 새도록
-                count2++;
                 //pop됐을때의 x좌표와 y좌표
                 int curx = cur.Item1;
                 int cury = cur.Item2;
@@ -53,69 +48,55 @@ namespace _4
                     curx = cur.Item1 + dx[i];
                     cury = cur.Item2 + dy[i];
                     //인덱스 범위를 벗어나면 continue
-                    if (curx < 0 || cury < 0 || curx > N || cury > N)
+                    if (curx < 0 || cury < 0 || curx > N || cury > M)
                         continue;
-                    //이미 방문했거나 해당 집이 0이면 continue
+                    //이미 방문했거나 해당 배추가 0이면 continue
                     if (visited[curx, cury] == true || map[curx, cury] == 0)
                         continue;
                     visited[curx, cury] = true;
                     queue.Enqueue((curx, cury));
                 }
             }
-            //단지내의 집 수 저장 나중에 오름차순 정렬해야함
-            houseCount[count - 1] = count2;
 
         }
         static void Main(string[] args)
         {
-            N = Convert.ToInt32(Console.ReadLine());
+            T = Convert.ToInt32(Console.ReadLine());
 
-            //map배열 초기화
-            for (int i = 1; i <= N; i++)
+            for (int m = 0; m < T; m++)
             {
-                string input = Console.ReadLine();
-                for (int j = 0; j < input.Length; j++)
+                int[] inputdata = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+                M = inputdata[0];
+                N = inputdata[1];
+                K = inputdata[2];
+
+                //방문,맵 배열 및 count초기화
+                Reset();
+
+                //배추 심기
+                for (int j = 0; j < K; j++)
                 {
-                    map[i, j + 1] = Convert.ToInt32((input[j].ToString()));
+                    int[] spot = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                    map[spot[1], spot[0]] = 1;
                 }
-            }
 
-            //방문 배열 초기화
-            Reset();
-
-            //나중에 단지내의 집수는 오름차순 정렬해야하므로
-            //미리 엄청 큰 수로 저장을 해놓으면 실제 단지내의 집 수가 들어왔을때
-            //그 숫자들이 앞으로 와서 정렬될 수 있도록 일부로 큰수로 초기화해둠
-            for (int i = 0; i < houseCount.Length; i++)
-            {
-                houseCount[i] = 1000000;
-            }
-
-            //map배열을 순회하면서 단지를 찾음
-            //단지를 찾는순간 bfs를 돌아 단지내의 집수가 몇인지 찾음
-            for (int i = 1; i <= N; i++)
-            {
-                for (int j = 1; j <= N; j++)
+                //map배열을 순회하면서 배추를 찾음
+                for (int j = 0; j < N; j++)
                 {
-                    if (map[i, j] == 1 && visited[i, j] == false)
+                    for (int k = 0; k < M; k++)
                     {
-                        //총 단지의 수 확인용
-                        count++;
-                        BFS(i, j, count);
+                        if (map[j, k] == 1 && visited[j, k] == false)
+                        {                            
+                            BFS(j, k);
+                            count++;
+                        }
                     }
                 }
+                Console.WriteLine(count);
             }
 
-            Console.WriteLine(count);
 
-            //오름차순 정렬
-            Array.Sort(houseCount);
-
-            //단지내의 집 수 출력
-            for (int i = 0; i < count; i++)
-            {
-                Console.WriteLine(houseCount[i]);
-            }
         }
     }
 }
