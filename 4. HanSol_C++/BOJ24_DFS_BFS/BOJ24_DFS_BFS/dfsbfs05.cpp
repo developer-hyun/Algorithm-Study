2a3bd32db255ad69c20b8abbb0614826c2dcfb64
한솔 <80498475+SolHaan@users.noreply.github.com>
2022-03-12 01:12:08 +0900

    BOJ 2178번 미로 탐색

--- 4. HanSol_C++/BOJ24_DFS_BFS/BOJ24_DFS_BFS/dfsbfs05.cpp
+++ 4. HanSol_C++/BOJ24_DFS_BFS/BOJ24_DFS_BFS/dfsbfs05.cpp
@@ -1,44 +1,50 @@
 //백준 2178번 미로 탐색
 /*
-
+1시간 넘김
+1012번 유기농 배추 문제와 다르게 bfs 사용해야 했고
+방문한 기록을 따로 배열을 만들어서 체크해줘야 했음
 */
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
-#define MAX 101
+#define MAX 100
 
 //intput
 int n, m;
 string map[MAX]; //인접 행렬 그래프
 
 //output
-int cnt = 1;
-vector<int> h;
+int visited[MAX][MAX]; //방문 여부
+queue<pair<int, int>> q;
 
 //x축 이동, y축 이동
 int dx[] = { 1,-1,0,0 };
 int dy[] = { 0,0,1,-1 };
-void dfs(int x, int y) {
-	cout << "x 값: " << x << ", y 값: " << y << "\n";
 
-	if (x == n - 1 && y == m - 1) return;
+//너비우선탐색
+void bfs(int x, int y) {
+	q.push(make_pair(x, y));
+	visited[x][y] = 1;
 
-	//상하좌우로 이동해서 1인 값을 찾기
-	for (int i = 0; i < 4; i++) {
-		int n_x = x + dx[i];
-		int n_y = y + dy[i];
+	while (!q.empty()) {
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
 
-		//범위를 넘어설때
-		if (n_x < 0 || n_x >= n || n_y < 0 || n_y >= m) continue;
+		//상하좌우로 이동해서 1인 값을 찾기
+		for (int i = 0; i < 4; i++) {
+			int n_x = x + dx[i];
+			int n_y = y + dy[i];
 
-		if (map[n_x][n_y] == '1') {
-			cnt++;
-			map[n_x][n_y] = '2'; //방문 체크
-			cout << "n_x 값: " << n_x << ", n_y 값: " << n_y << "\n";
-			dfs(n_x, n_y);
+			//범위를 넘어설때
+			if (n_x < 0 || n_x >= n || n_y < 0 || n_y >= m) continue;
+
+			if (visited[n_x][n_y] == 0 && map[n_x][n_y] == '1') {
+				q.push(make_pair(n_x, n_y));
+				visited[n_x][n_y] = visited[x][y] + 1; //방문 체크
+			}
 		}
 	}
 }
@@ -52,10 +58,9 @@ int main()
 		cin >> map[i];
 	}
 
-	map[0][0] = '2'; //방문체크
-	dfs(0, 0);
+	bfs(0, 0);
 
-	cout << cnt << "\n";
+	cout << visited[n-1][m-1] << "\n";
 
 	return 0;
 }
